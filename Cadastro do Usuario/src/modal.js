477d3f14df12a33465907593ad32ebf4dddf6d2c
Caue de Freitas Adriano <caue.adriano@outlook.com>
2024-06-19 09:51:09 -0300

    Conclusão da página de portfolio
        Renomeei arquivo JS dos modais
        criei o modal para as opções compartilhar e denunciar

--- Cadastro do Usuario/src/modal.js
+++ Cadastro do Usuario/src/modal.js
@@ -1,3 +1,23 @@
+//Modal Compartilhar e denunciar
+
+var menuModal = document.getElementById("menu-modal")
+var btnMenu = document.getElementById("user-btns-more")
+
+btnMenu.onclick = function() {
+    menuModal.style.display = "block";
+    console.log("modal aberto")
+}
+
+menuModal.onclick = function(event) {
+    if (event.target == menuModal) {
+        console.log("testando")
+        menuModal.style.display = "none";
+    }
+    
+}
+
+//=-=-=-=-=-=-=-=Modal dos links=-=-=-=-=-=-=-=-=-=//
+
 var modal = document.getElementById("links-modal")
 var btn = document.getElementById("linkBtn")
 var span = document.getElementsByClassName("close")[0]
@@ -5,7 +25,7 @@ var span = document.getElementsByClassName("close")[0]
 //quando o usuário apertar o botão, abre o modal
 btn.onclick = function() {
     modal.style.display = "block";
-  }
+}
 
 //quando o usuário apertar o span X, fecha o modal
 span.onclick = function() {
